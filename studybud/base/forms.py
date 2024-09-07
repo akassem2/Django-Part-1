ddc11aef5575aec79fb9227a882f5a7dc317f075
akassem2 <akassem7977@gmail.com>
2024-09-07 17:46:09 -0400

    CRUD
    Reached the 2:06:22 timestamp

---
+++ studybud/base/forms.py
@@ -0,0 +1,7 @@
+from django.forms import ModelForm
+from .models import Room
+
+class RoomForm(ModelForm): #Creates a form based on the Room Model and its fields in models.py
+    class Meta:
+        model = Room
+        fields =  '__all__'
