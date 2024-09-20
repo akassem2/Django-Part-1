f292e00fb886206d2ab749c02282d16ec9b4ba2a
akassem2 <akassem7977@gmail.com>
2024-09-19 20:40:39 -0400

    Edit User Account Page
    Reached the 5:35:02 timestamp.
    Fixed up the themes for the page that lets the user edit their name and email, as well as the login and register page.

--- studybud/base/forms.py
+++ studybud/base/forms.py
@@ -1,8 +1,14 @@
 from django.forms import ModelForm
 from .models import Room
+from django.contrib.auth.models import User
 
 class RoomForm(ModelForm): #Creates a form based on the Room Model and its fields in models.py
     class Meta:
         model = Room
         fields =  '__all__'
         exclude = ['host', 'participants']
+
+class UserForm(ModelForm):
+    class Meta:
+        model = User
+        fields = ['username', 'email']
